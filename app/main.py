3eab7471542840f4a54c2ac574b21be748a9f1bf
mutt0-ds <davide.muttoni@pharus.ch>
2021-10-07 09:01:54 +0200

    Initial Commit

--- app/main.py
+++ app/main.py
@@ -5,30 +5,34 @@ import pandas as pd
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET', 'POST'])
-def hello():
-    # players = main()
-    with open("static/test.json") as op:
-        players = json.load(op)
-    # testo = "<h1>Hello World!</h1><br><h2>My Team:</h2><br>"
-    # testo+="<br>".join(list(players.keys()))
-    return jsonify(players)
+# @app.route('/', methods=['GET', 'POST'])
+# def hello():
+#     # players = main()
+#     with open("static/test.json") as op:
+#         players = json.load(op)
+#     # testo = "<h1>Hello World!</h1><br><h2>My Team:</h2><br>"
+#     # testo+="<br>".join(list(players.keys()))
+#     return jsonify(players)
 
-@app.route('/home', methods=['GET', 'POST'])
-def home():
-    with open("static/test.json") as op:
-        players = json.load(op)
-        df = pd.DataFrame.from_dict(players).T
-        # df["news"] = ["\n".join(x) for x in df["news"]]
-        html_df = df.to_html(index=False).replace('class="dataframe"','')
+# @app.route('/home', methods=['GET', 'POST'])
+# def home():
+#     with open("static/test.json") as op:
+#         players = json.load(op)
+#         df = pd.DataFrame.from_dict(players).T
+#         # df["news"] = ["\n".join(x) for x in df["news"]]
+#         html_df = df.to_html(index=False).replace('class="dataframe"','')
         
-    # return render_template('test.html', title="home",table=html_df)
-    #within app.route add the html page we are doing changes to
-    return render_template("test.html", column_names=df.columns.values, row_data=list(df.values.tolist()),
-                           link_column="SCHEDA GIOCATORE",news="NEWS", zip=zip)
+#     # return render_template('test.html', title="home",table=html_df)
+#     #within app.route add the html page we are doing changes to
+#     return render_template("test.html", column_names=df.columns.values, row_data=list(df.values.tolist()),
+#                            link_column="SCHEDA GIOCATORE",news="NEWS", zip=zip)
+
+@app.route('/')
+def index():
+    return "<h1>Welcome to our server !!</h1>"
 
 if __name__ == '__main__':
-    app.run(host="localhost", port=8000, debug=True,)
+    app.run(host="localhost", port=8000, debug=True)
 
 #TODO: https://stackoverflow.com/questions/52644035/how-to-show-a-pandas-dataframe-into-a-existing-flask-html-table
 
