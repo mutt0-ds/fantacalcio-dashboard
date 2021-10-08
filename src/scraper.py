da90014d8e51866357646bee82e036a029069e95
mutt0-ds <davide.muttoni@pharus.ch>
2021-10-08 15:22:48 +0200

    messo task refresh

--- src/scraper.py
+++ src/scraper.py
@@ -1,7 +1,5 @@
 import json
 import src.scraper_utils as utils
-import os
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 def main():
     LINK_SOS_FANTA="https://sosfanta.calciomercato.com/category/chi-schierare/"
@@ -9,8 +7,7 @@ def main():
     LINK_FANTACALCIO_IT = "https://www.fantacalcio.it/squadre/"
 
     #lista_giocatori (è un dizionario vuoto)
-    print(os.getcwd())
-    data = json.load(open(BASE_DIR+"/app/static/team.json")).get("players")
+    data = json.load(open("/static/team.json")).get("players")
 
     contenuto_articoli= utils.estrazione_articoli_da_leggere(LINK_SOS_FANTA)
     liste_status =  utils.estrazione_formazioni(PROBABILI_FORMAZIONI)
@@ -28,7 +25,7 @@ def main():
     for key in dictio_finale:
         dictio_finale[key] = { k.replace('_', ' ').upper(): v for k, v in dictio_finale[key].items() }
 
-    with open(BASE_DIR+'/app/static/test.json', 'w') as fp:
+    with open('/static/test.json', 'w') as fp:
         json.dump(dictio_finale, fp)
 
     print("oplà")
